12efd246e9e89b0bdf311010f7d9fd28cbe16bee
Raul B <boulocqraul@gmail.com>
2022-08-04 12:30:27 -0400

    worked on checkout page

--- src/app/App.js
+++ src/app/App.js
@@ -4,6 +4,15 @@ import HomePage from '../homepage/Homepage';
 import Header from '../homepage/header/Header';
 import NotFound from './NotFound';
 import SellerPage from './components/seller/SellerPage';
+import CheckoutPage from './components/CheckoutPage';
+//
+import 'bootstrap/dist/css/bootstrap.min.css';
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
+  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
+  crossorigin="anonymous"
+/>
 
 /**
  * @name App
@@ -18,6 +27,7 @@ const App = () => {
       <Route exact path="/"   element={<HomePage /> } />
        <Route path="*" element={<NotFound />} />
        <Route path="seller" element={<SellerPage />} />
+       <Route path="checkout" element={<CheckoutPage/>}/>
    
     </Routes>
     {/* </Header> */}
