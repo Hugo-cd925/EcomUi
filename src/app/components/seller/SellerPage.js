8c51b26b4033436fed9d74ad6c2a41f2400dae6b
Hugo Delgado <hugo.cdelgado925@gmail.com>
2022-08-05 10:58:22 -0700

    add changes

--- src/app/components/seller/SellerPage.js
+++ src/app/components/seller/SellerPage.js
@@ -9,7 +9,7 @@ const SellerPage = () => {
     const [apiError, setApiError] = useState(false);
 
     useEffect(() => {
-        fetchUserById(setUser, 7, setApiError);
+        fetchUserById(setUser, 1, setApiError);
         // fetchUsers(setUser, setApiError);
     }, []);
 
@@ -47,7 +47,7 @@ const SellerPage = () => {
                     {/* {user && user.map((obj) => (
                 <div>{obj.FirstName}</div>
                ))} */}
-                    {user.FirstName}
+                    {user.firstName}
                     Seller Info displayed
                 </div>
                 <div>
