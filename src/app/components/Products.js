941c2bcaa7d3455fe721dfdec6f613bfbf47c832
Raul B <boulocqraul@gmail.com>
2022-08-07 19:50:22 -0400

    Products page completed

---
+++ src/app/components/Products.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Products() {
+  const url = "https://localhost:7218/api/Products";
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setProducts(response.data);
+    });
+  }, [url]);
+  if (products) {
+    return (
+      <div className="container mt-2 mb-1">
+        <h1 className="m-5 text-center">Products Page</h1>
+        <div className="row mt-5">
+          {products.map((prod, index) => {
+            return (
+              <div 
+              key={index}
+              className="col-md-3">
+                  <a
+                    href="#"
+                    className="text-decoration-none text-dark text-center"
+                  >
+                    <div className="d-flex justify-content-center align-items-center-center">
+                        <img
+                        className="mx-auto"
+                        style={{ width: "65%" }}
+                        src={prod.img}
+                        alt={`Picture of ${prod.name}`}
+                    />
+                    </div>
+                        <div className="mt-3">{prod.name}</div>
+                  </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+  return <></>;
+}
+
+export default Products;
