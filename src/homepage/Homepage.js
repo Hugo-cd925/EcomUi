f9559300dc2fe4d78315a35c9b79d262268f6019
Hugo Delgado <hugo.cdelgado925@gmail.com>
2022-08-09 13:11:28 -0700

    fix login

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import css from "../app/App.css";
-
+import GoogleLogin from '../app/components/google/GoogleLogin';
 const HomePage = () => {
   return (
     <div>
       <div className='container'>
       {/* <video src='/videos/video-1.mp4' autoPlay loop muted /> */}
+      <GoogleLogin />
       <h1>Buyer or Seller</h1>
+
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
         <button
@@ -34,5 +36,7 @@ const HomePage = () => {
     </div>
   );
 };
+
+export default HomePage
 
 
